de537cd620bb0b0ea8836b85898ec591cb7c1b1d
morgantatums <morgantatums@zillowgroup.com>
2019-10-05 14:57:45 -0700

    added comments to js file

--- challenges/arrayBinarySearch/array-binary-search.js
+++ challenges/arrayBinarySearch/array-binary-search.js
@@ -1,13 +1,18 @@
 'Use Strict';
-const arrayBinary = {};
-arrayBinary.BinarySearch = (array, input)=>{
+
+//this is the function that will search an array for a given input
+const BinarySearch = (array, input)=>{
+  //answer by default will be -1 and that value will only change if the input exists in the array
   let answer = -1;
+  //loops through the array and searches for the input value
   for (let i = 0; i < array.length; i++){
     if(array[i] === input){
+      //if the input exists, update answer variable
       answer = i;
     }
   }
+  //return the answer which is -1 if input DNE or the index of the input
   return answer;
 };
 
-module.exports = arrayBinary;
+module.exports = BinarySearch;
