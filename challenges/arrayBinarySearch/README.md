6961567de0d9e0312f4a206643cc411ec06fb6c8
morgantatums <morgantatums@zillowgroup.com>
2019-10-05 18:14:45 -0700

    added UML

--- challenges/arrayBinarySearch/README.md
+++ challenges/arrayBinarySearch/README.md
@@ -1,13 +1,18 @@
-# Reverse an Array
+# Author:
+Morgan Shaw
+Nadya Ilinskaya
+James Dunn
+
+# Binary Search
 A function called BinarySearch which takes in 2 parameters: a sorted array and the search key. It returns the index of the array’s element that is equal to the search key, or -1 if the element does not exist.
 
 ## Challenge
 Write a function called BinarySearch which takes in 2 parameters: a sorted array and the search key. Without utilizing any of the built-in methods available to your language, return the index of the array’s element that is equal to the search key, or -1 if the element does not exist.
 
 ## Approach & Efficiency
-
+While loop through the array: Find the middle of the array. Compare the value that is located at the index = middle to the search key. Because the array is sorted we can determine to change our search parameters to the left or the right of the array. Once the index is found return the index, if the key does not exist in the array return -1. 
 
 ## Solution
 [link](./array-binary-search.js)
 [Repl.it](https://repl.it/repls/OrneryCurlyDividend)
-![UML Diagram]()
+![UML Diagram](../../assets/CodeChallenge3.jpg)
--- challenges/arrayBinarySearch/array-binary-search.js
+++ challenges/arrayBinarySearch/array-binary-search.js
@@ -1,18 +1,25 @@
 'Use Strict';
 
 //this is the function that will search an array for a given input
-const BinarySearch = (array, input)=>{
-  //answer by default will be -1 and that value will only change if the input exists in the array
-  let answer = -1;
-  //loops through the array and searches for the input value
-  for (let i = 0; i < array.length; i++){
-    if(array[i] === input){
-      //if the input exists, update answer variable
-      answer = i;
+const BinarySearch = (arr, search)=>{
+  //check if middle index matches key
+  let firstIndex = 0;
+  let lastIndex = arr.length - 1;
+  while (firstIndex <= lastIndex) {
+    let middle = Math.floor((firstIndex + lastIndex) / 2);
+    if (arr[middle] === search) {
+      return middle;
+    }
+    if (arr[middle] > search) {
+    // if search is less, then firstIndex stays same
+    // and lastIndex changes to middle minus 1
+      lastIndex = --middle;
+    // divide that by two, and make that new middle
+    } else if (arr[middle] < search) {
+      firstIndex = ++middle;
     }
   }
-  //return the answer which is -1 if input DNE or the index of the input
-  return answer;
+  return -1;
 };
 
 module.exports = BinarySearch;
