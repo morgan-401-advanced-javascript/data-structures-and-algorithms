9f03e1db2e789c148069bf28e7bae984778d5524
morgantatums <morgantatums@zillowgroup.com>
2019-10-16 20:11:08 -0700

    added additional test

--- challenges/LinkList/__tests__/linked-list.test.js
+++ challenges/LinkList/__tests__/linked-list.test.js
@@ -25,10 +25,31 @@ describe('insert into the linked list', ()=>{
   });
 
 });
-
-
 // * The head property will properly point to the first node in the linked list
+describe('The head property will properly point to the first node in the linked list', ()=>{
+  let list = new LinkedList();
+  list.insert('yellow');
+  list.insert('blue');
+
+  it('first node is correct', ()=>{
+    expect(list.head.value).toBe('blue');
+  });
+
+});
 // * Can properly insert multiple nodes into the linked list
+describe('Can properly insert multiple nodes into the linked list', ()=>{
+  let list = new LinkedList();
+  list.insert('yellow');
+  list.insert('blue');
+
+  it('successfully added head', ()=>{
+    expect(list.head.value).toBe('blue');
+  });
+  it('successfully added 2nd value', ()=>{
+    expect(list.head.next.value).toBe('yellow');
+  });
+});
+
 // * Will return true when finding a value within the linked list that exists
 // * Will return false when searching for a value in the linked list that does not exist
 // * Can properly return a collection of all the values that exist in the linked list
