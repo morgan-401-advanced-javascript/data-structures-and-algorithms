e3940c72cefb667edb3dfbe121ab5a76eaae5a75
morgantatums <morgantatums@zillowgroup.com>
2019-10-16 20:01:37 -0700

    added 2 tests

--- challenges/LinkList/__tests__/linked-list.test.js
+++ challenges/LinkList/__tests__/linked-list.test.js
@@ -1,8 +1,34 @@
-const LinkList = require('../linked-list.js');
+const LinkedList = require('../linked-list.js');
 
 describe('Test', () => {
-  // How might we repeat this to check on types?
   it('true', ()=>{
     expect(true).toBeTruthy();
   });
 });
+
+// * Can successfully instantiate an empty linked list
+describe('instantiate an empty linked list', ()=>{
+  let list = new LinkedList();
+  it('empty list',()=>{
+    expect(list).toBeTruthy();
+  });
+});
+
+// * Can properly insert into the linked list
+describe('insert into the linked list', ()=>{
+  let list = new LinkedList();
+  list.insert('yellow');
+
+  it('successfully inserts into list', ()=>{
+    expect(list.head.value).toBe('yellow');
+
+  });
+
+});
+
+
+// * The head property will properly point to the first node in the linked list
+// * Can properly insert multiple nodes into the linked list
+// * Will return true when finding a value within the linked list that exists
+// * Will return false when searching for a value in the linked list that does not exist
+// * Can properly return a collection of all the values that exist in the linked list
--- challenges/LinkList/linked-list.js
+++ challenges/LinkList/linked-list.js
@@ -1,7 +1,22 @@
 const Node = require('./node.js');
 
+// instantiate an empty linked list
 class LinkedList {
   constructor(){
     this.head = null;
   }
+  // insert item into the beginning of the list
+  insert(value){
+    let inserted = new Node(value);
+    inserted.next = this.head;
+    this.head = inserted;
+    return this.head;
+  }
 }
+// * The head property will properly point to the first node in the linked list
+// * Can properly insert multiple nodes into the linked list
+// * Will return true when finding a value within the linked list that exists
+// * Will return false when searching for a value in the linked list that does not exist
+// * Can properly return a collection of all the values that exist in the linked list
+
+module.exports = LinkedList;
