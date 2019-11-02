80dae551d02d49b3a9ed3b47e8f9d4eab3b579d8
morgantatums <morgantatums@zillowgroup.com>
2019-11-02 10:26:19 -0700

    tried to fix lint

--- Data-Structures/LinkList/__tests__/linked-list.test.js
+++ Data-Structures/LinkList/__tests__/linked-list.test.js
@@ -3,6 +3,6 @@ const LinkList = require('../linked-list.js');
 xdescribe('Test', () => {
   // How might we repeat this to check on types?
   it('true', ()=>{
-    expect(true).toBeTruthy();
+    expect(LinkList).toBeTruthy();
   });
 });
