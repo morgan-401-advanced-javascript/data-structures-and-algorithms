dc860e09d7f38eb3134f3068c8a07285824972dc
morgantatums <morgantatums@zillowgroup.com>
2019-11-07 17:49:31 -0800

    finished final functions

--- challenges/fifoAnimalShelter/__tests__/fifo-animal-shelter.test.js
+++ challenges/fifoAnimalShelter/__tests__/fifo-animal-shelter.test.js
@@ -6,20 +6,75 @@ const AnimalShelter =  fifoAnimalShelter.AnimalShelter;
 describe('Fifo Animal Shelter Tests!', () => {
   // Can successfully enqueue a Dog
   it('Can successfully enqueue a Dog', ()=>{
-    let fido = 1;
-    // fido.name = 'fido';
+    let dog = new Dog('Hachi');
     let home = new AnimalShelter;
-    home.enqueue(fido);
-    console.log(home);
-
-    expect(home.front.data.name).toBe('fido');
+    home.enqueue(dog);
+    expect(home.front.data.name).toBe('Hachi');
   });
-});
+  // Can successfully enqueue a Cat
+  it('Can successfully enqueue a Cat', ()=>{
+    let cat = new Cat('Luna');
+    let home = new AnimalShelter;
+    home.enqueue(cat);
+    expect(home.front.data.name).toBe('Luna');
+  });
+  
+  // Error handling when you try to enqueue something that is neither a Dog nor a Cat
+  it('Error handling when you try to enqueue something that is neither a Dog nor a Cat', ()=>{
+    let kangaroo = 'yellow kangaroo';
+    let home = new AnimalShelter;
+    expect(home.enqueue(kangaroo)).toBe('Cats & Dogs only!');
+  });
 
-// Can successfully enqueue a Cat
-// Error handling when you try to enqueue something that is neither a Dog nor a Cat
-// Can successfully dequeue a Dog
-// Can successfully dequeue a Cat
-// Can successfully dequeue the front of the AnimalShelter queue when you pass no parameters to dequeue
-// Error handling when you try to dequeue something that is neither a 'cat', 'dog' or an empty string
-// Can successfully print the resulting object from a dequeue action
+  // Can successfully dequeue a Dog
+  it('Can successfully dequeue a Dog', ()=>{
+    let dog = new Dog('Hachi');
+    let cat = new Cat('Luna');
+    let home = new AnimalShelter;
+    home.enqueue(dog);
+    home.enqueue(cat);
+    expect(home.dequeue('Dog')).toBe('Hachi is a good dog!');
+  });
+
+  // Can successfully dequeue a Cat
+  it('Can successfully dequeue a Cat', ()=>{
+    let cat = new Cat('Luna');
+    let cat2 = new Cat('Mao');
+    let dog = new Dog('Hachi');
+    let home = new AnimalShelter;
+    home.enqueue(cat);
+    home.enqueue(cat2);
+    home.enqueue(dog);
+    home.dequeue('Dog');
+    console.log(home);
+    expect(home.dequeue('Cat')).toBe('Luna is a sweet cat!');
+  });
+
+  // Can successfully dequeue the front of the AnimalShelter queue when you pass no parameters to dequeue
+  it('Can successfully dequeue the front of the AnimalShelter queue when you pass no parameters to dequeue', ()=>{
+    let cat = new Cat('Luna');
+    let dog = new Dog('Hachi');
+    let home = new AnimalShelter;
+    home.enqueue(cat);
+    home.enqueue(dog);
+    
+    expect(home.dequeue()).toBe('Luna is a sweet cat!');
+  });
+
+  // Error handling when you try to dequeue something that is neither a 'cat', 'dog' or an empty string
+  it('Error handling when you try to dequeue something that is neither a "cat" or "dog" or an empty string', ()=>{
+    let cat = new Cat('Luna');
+    let home = new AnimalShelter;
+    home.enqueue(cat);
+    expect(home.dequeue('lizard')).toBe('we do not have that animal');
+  });
+
+  // Can successfully print the resulting object from a dequeue action
+  it('Can successfully print the resulting object from a dequeue action', ()=>{
+    let cat = new Cat('Luna');
+    let home = new AnimalShelter;
+    home.enqueue(cat);
+    expect(home.dequeue('Cat')).toBe('Luna is a sweet cat!');
+  });
+
+});
--- challenges/fifoAnimalShelter/fifo-animal-shelter.js
+++ challenges/fifoAnimalShelter/fifo-animal-shelter.js
@@ -2,20 +2,20 @@
 const Node = require('./node.js');
 
 class Dog {
-  constructor(){
-    this.name = '';
+  constructor(name){
+    this.name = name;
   }
   print(){
-    return this.name + ' is a good dog!';
+    return `${this.name} is a good dog!`;
   }
 }
 
 class Cat {
-  constructor(){
-    this.name = '';
+  constructor(name){
+    this.name = name;
   }
   print(){
-    return this.name + ' is a sweet cat!';
+    return `${this.name} is a sweet cat!`;
   }
 }
 
@@ -25,14 +25,18 @@ class AnimalShelter {
     this.rear = null;
   }
   enqueue(animal){
-    if(animal === Cat || Dog){
+    let species = animal.constructor.name;
+
+    if(species === 'Cat'|| species === 'Dog'){
       let newItem = new Node(animal);
       if(this.rear === null){
         this.rear = newItem;
         this.front = newItem;
+        return;
       }else{
         this.rear.next = newItem;
         this.rear = newItem;
+        return;
       }
     }else{
       return 'Cats & Dogs only!';
@@ -40,8 +44,36 @@ class AnimalShelter {
   }
   dequeue(pref){
 
+    let current = this.front;
+
+    if(current.data.constructor.name === pref || pref === undefined){
+      this.temp = this.front;
+      this.front = this.temp.next;
+      return this.temp.data.print();
+    }
+    while(current.next){
+      if(current.next.data.constructor.name === pref){
+        if(current.next === this.rear){
+          this.temp = current.next;
+          current.next= current;
+          this.rear = current;
+          return this.temp.data.print();
+        }
+        this.temp = current.next;
+        current.next = current.next.next;
+        return this.temp.data.print();
+      } 
+      current = current.next;
+    }
+    return 'we do not have that animal';
   }
+  
+    
+
+
+
 }
+
 
 module.exports =  { Dog, Cat, AnimalShelter};
 
