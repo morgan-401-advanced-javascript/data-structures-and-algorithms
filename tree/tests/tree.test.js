fb2f13da0cce98a9e5c36852a8496cdc2e7b6576
morgantatums <morgantatums@zillowgroup.com>
2019-12-12 19:01:34 -0800

    set up repo & file structure

--- tree/tests/tree.test.js
+++ tree/tests/tree.test.js
@@ -1,20 +1,21 @@
-'use strict';
+// const BinarySearchTree = require('../binary-search.js');
+const BinaryTree = require('../tree.js');
 
-const BinarySearchTree = require('../binary-search.js');
-
-let tree = new BinarySearchTree;
 
 describe('Binary Search Tree', () => {
   it('Can successfully instantiate an empty tree', () => { 
+    let tree = new BinaryTree;
     expect(tree).toBeTruthy();
   }); 
   
   it('Can successfully instantiate a tree with a single root node', () => { 
+    let tree = new BinaryTree;
     tree.add(5);
     expect(tree.root.data).toBe(5);
   }); 
-
+  
   it('Can successfully add a left child and right child to a single root node on a Binary Search Tree', () => { 
+    let tree = new BinaryTree;
     tree.add(5);
     tree.add(7);
     tree.add(2);
@@ -23,8 +24,16 @@ describe('Binary Search Tree', () => {
     expect(tree.root.right.data).toBe(7);
   }); 
 });
-
-
+describe('Search Tree', () => {
+  let tree = new BinaryTree;
+  tree.add(2);
+  tree.add(1);
+  tree.add(3);
+  console.log(tree.preOrder());
+  it('Can successfully return a collection from a pre-order traversal', () => { 
+    expect(tree.preOrder()).toBeTruthy();
+  }); 
+});
 // Can successfully instantiate an empty tree
 // Can successfully instantiate a tree with a single root node
 // Can successfully return a collection from a pre-order traversal
--- tree/tree.js
+++ tree/tree.js
@@ -5,16 +5,124 @@ class BinaryTree {
   // A root property that stores a Node representing the start of the tree
   constructor() {
     this.root = null;
-    this.arr = [];
   }
   // A preOrder method that traverses the tree using the pattern root >> left >> right and returns an array of the traversed values
 
-  preOrder(node) {
+  preOrder() {
+    let treeRoot = this.root;
+    let array = [];
+    array.push(treeRoot.data);
 
+    if (treeRoot.left) {
+
+      
+      array.push(...treeRoot.left.preOrder());
+    
+    }
+
+    if (treeRoot.right) {
+      array.push(...treeRoot.right.preOrder());
+    }
+    return array;
+  }
+
+  inOrder() {
+    let treeRoot = this.root;
+    let array = [];
+
+    if (treeRoot.left) {
+      array.push(...treeRoot.left.inOrder());
+    }
+
+    array.push(treeRoot.value);
+
+    if (treeRoot.right) {
+      array.push(...treeRoot.right.inOrder());
+    }
+    return array;
+  }
+
+  postOrder() {
+    let treeRoot = this.root;
+    let array = [];
+
+    if (treeRoot.left) {
+      array.push(...treeRoot.left.postOrder());
+    }
+
+    if (treeRoot.right) {
+      array.push(...treeRoot.right.postOrder());
+    }
+
+    array.push(treeRoot.value);
+    return array;
+
+  }
+
+}
+
+class BinarySearchTree extends BinaryTree {
+  add(value) {
+    let node = this.root;
+
+    // if a root does not exist
+    // set node as root
+    if(!node) {
+      this.root = new Node(value);
+      return;
+    }
+    else {
+      const search = function(node) {
+        // if the value is less than the root
+        // checking node.left
+        if(value < node.data) {
+          if(node.left === null) {
+            node.left = new Node(value);
+            return;
+          }
+          else {
+            return search(node.left);
+          }
+        }
+
+        // if value is greater than the root
+        // checking node.right
+        else if (value > node.data) {
+          if(node.right === null) {
+            node.right = new Node(value);
+            return;
+          }
+          else {
+            return search(node.right);
+          }
+        } else {
+          return null;
+        }
+      };
+
+      return search(node);
+    }
+  }
+
+  contains(value) {
+    let current = this.root;
+    while(current) {
+      if(value === current.data) {
+        return true;
+      }
+      if(value < current.data) {
+        current= current.left;
+      }
+      else {
+        current = current.right;
+      }
+    }
+    
+    return false;
   }
 }
 
-module.exports = BinaryTree;
+module.exports = BinarySearchTree;
 
 
 // An inOrder method that traverses the tree using the pattern left >> root >> right and returns an array of the traversed values
