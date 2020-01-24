d76406b0fd163d8edd19c99247ae5ffdcaa0c959
morgantatums <morgantatums@zillowgroup.com>
2020-01-23 18:09:22 -0800

    finished code

---
+++ challenges/treeIntersection/node.js
@@ -0,0 +1,11 @@
+'use strict';
+
+class Node {
+  constructor(value) {
+    this.data = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+module.exports = Node;
---
+++ challenges/treeIntersection/tree-intersection.js
@@ -0,0 +1,30 @@
+'use strict';
+const node = require('./node');
+const tree = require('./tree');
+
+
+function tree_intersection(tree1, tree2){
+    if(!tree1.root || !tree2.root){
+        return 'invalid';
+    }
+  // loop through tree 1
+  let tree1Array = tree1.inOrder();
+  let tree2Array = tree2.inOrder();
+
+  let newArray = [];
+
+  for(let i = 0; i < tree1Array.length; i ++){
+    // compare each node to tree2
+    for(let j = 0; j <tree2Array.length; j++){
+      // push dupes to an array
+      if(tree1Array[i] === tree2Array[j]){
+        newArray.push(tree1Array[i]);
+      }
+    }
+  }
+  return newArray;
+
+
+}
+
+module.exports = tree_intersection;
